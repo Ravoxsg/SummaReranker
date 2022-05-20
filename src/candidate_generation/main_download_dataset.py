f0155507cd57a8fb0890b907042f90b8c1d127a4
Mathieu <mathieu.ravaut@gmail.com>
2022-05-20 23:06:18 +0800

    base

---
+++ src/candidate_generation/main_download_dataset.py
@@ -0,0 +1,131 @@
+# Generate summary candidates with the fine-tuned models.
+
+import time
+import argparse
+import sys
+
+sys.path.append("/data/mathieu/CODE_RELEASES/SummaReranker/src/") # todo: change to your folder path
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--seed', type = int, default = 42)
+
+# data
+parser.add_argument('--dataset', type=str, default = "reddit", choices= ["cnndm", "xsum", "reddit"])
+parser.add_argument('--data_folder', type = str, default = "/data/mathieu/DATASETS/RedditTIFU/data/en2/") # todo: change to where you want to save the data
+
+args = parser.parse_args()
+
+datasets_names = ["cnndm", "xsum", "reddit"]
+dataset_names = ["cnn_dailymail", "xsum", "reddit_tifu"]
+make_splits = [False, False, True]
+data_versions = ["3.0.0", "", "long"]
+text_keys = ["article", "document", "documents"]
+summary_keys = ["highlights", "summary", "tldr"]
+
+idx = datasets.index(args.dataset)
+args.dataset_name = dataset_names[idx]
+args.make_split = make_splits[idx]
+args.data_version = data_versions[idx]
+args.text_key = text_keys[idx]
+args.summary_key = summary_keys[idx]
+
+print("*"*50)
+print(args)
+
+sets = [("validation", "val"), ("test", "test"), ("train", "train")]
+contents = [(args.text_key, "text"), (args.summary_key, "summary")]
+
+
+
+def main(args):
+    seed_everything(42)
+
+    data_path = "data/en/"
+    if args.dataset in ["xsum"]:
+        dataset = datasets.load_dataset(args.dataset_name)
+    else:
+        dataset = datasets.load_dataset(args.dataset_name, args.data_version)
+
+    if args.make_split:
+        dataset = dataset["train"]
+
+        texts = [x[args.text_key] for x in dataset]
+        summaries = [x[args.summary_key] for x in dataset]
+
+        idx = np.random.permutation(len(texts))
+        texts = [texts[i] for i in idx]
+        summaries = [summaries[i] for i in idx]
+
+        print(len(texts), len(summaries))
+        print(texts[0])
+        print("*" * 50)
+        print(summaries[0])
+
+        thresh = int(0.1 * len(texts))
+        train_texts = texts[:(8 * thresh)]
+        train_summaries = summaries[:(8 * thresh)]
+        val_texts = texts[(8 * thresh):(9 * thresh)]
+        val_summaries = summaries[(8 * thresh):(9 * thresh)]
+        test_texts = texts[(9 * thresh):]
+        test_summaries = summaries[(9 * thresh):]
+        print(len(train_texts), len(val_texts), len(test_texts))
+
+        set_texts = [train_texts, val_texts, test_texts]
+        set_summaries = [train_summaries, val_summaries, test_summaries]
+        set_names = ["train", "val", "test"]
+        idx = 0
+        for set_name in set_names:
+            text = set_texts[idx]
+            text_path = args.data_folder + "/{}_text.txt".format(set_name)
+            write_to_txt(text, text_path)
+
+            summary = set_summaries[idx]
+            summary_path = args.data_folder + "/{}_summary.txt".format(set_name)
+            write_to_txt(summary, summary_path)
+
+            print(set_name, len(text), len(summary))
+            idx += 1
+    else:
+        for x in sets:
+            (set, set_name) = x
+            for y in contents:
+                (content, content_name) = y
+                print(set, set_name, content, content_name)
+                dataset_set = dataset[set]
+
+                text = [x[content].replace("\n", " ") for x in dataset_set]
+                print(set, len(text))
+
+                path = data_path + "/{}_{}.txt".format(set_name, content_name)
+                write_to_txt(text, path)
+
+                if set != "train":
+                    text = [x[content] for x in dataset_set]
+                    print(set, len(text))
+
+                    folder_path = data_path + set_name + "/" + content_name + "/"
+                    write_to_individual_txt(text, folder_path, set_name, content_name)
+
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
+
+def write_to_txt(l, path):
+    with open(path, "w") as f:
+        for i in tqdm(range(len(l))):
+            text = l[i]
+            text = text.replace("\n", " ").replace("\t", " ").replace("\r", " ")
+            if len(text) == 0:
+                print(i)
+            f.write(text + "\n")
+
+
+if __name__ == '__main__':
+
+    main(args)
