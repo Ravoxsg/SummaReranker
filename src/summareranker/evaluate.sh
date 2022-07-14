14f99827ee9ddc6e9e423f8bdbb5ed077eb03867
Ravoxsg <mathieu.ravaut@gmail.com>
2022-07-14 15:28:00 +0800

    base

--- src/summareranker/evaluate.sh
+++ src/summareranker/evaluate.sh
@@ -6,7 +6,7 @@ python evaluate.py \
 --model_name pegasus_reddit_train_1 \
 --num_beams 15 \
 --model roberta-large \
---cache_dir /data/mathieu/hf_models/roberta-large \
+--cache_dir ../../../hf_models/roberta-large \
 --load_model False \
 --load_model_path /data/mathieu/2nd_stage_summarization/4_supervised_multitask_reranking/saved_models/reddit/multitask_3_tasks_ablation_5/checkpoint-1000/pytorch_model.bin \
 --inference_bs 60 \
